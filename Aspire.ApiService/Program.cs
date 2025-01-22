3b956a63423f7a0a3276464f77be27445ffadab0
KishanP <Kparmar@magnusminds.net>
2025-01-22 11:41:13 +0530

    Add project files.

---
+++ Aspire.ApiService/Program.cs
@@ -0,0 +1,54 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+// Add services for Scalar OpenAPI documentation
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+
+string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+app.MapGet("/weatherforecast", () =>
+{
+    Random rand = new Random();
+    int errorInt = rand.Next(1, 4);
+    if (errorInt == 3)
+    {
+        throw new Exception("Error while fetching the weatherforecast API.");
+    }
+    Console.WriteLine("*** Console log *** fetching the records from weatherforecast API");
+    app.Logger.LogInformation("*** Ilogger *** fetching the records from weatherforecast API");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapDefaultEndpoints();
+
+app.MapOpenApi();
+
+app.MapScalarApiReference();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
